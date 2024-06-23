e77ce4993d8e0abbc3686f339e1127c1774735bb
mcMineyC <75390386+mcMineyC@users.noreply.github.com>
2024-06-22 21:11:56 -0700

    Update handler_just_audio.dart

--- lib/src/handler_just_audio.dart
+++ lib/src/handler_just_audio.dart
@@ -80,6 +80,7 @@ class AudioHandlerJustAudio extends BaseAudioHandler
     
     await _player.setAudioSource(AudioSource.uri(Uri.parse(item.id)), initialPosition: Duration(seconds: 0));
     await _player.play();
+    mediaItem.add(mediaItem);
   }
 
   @override
